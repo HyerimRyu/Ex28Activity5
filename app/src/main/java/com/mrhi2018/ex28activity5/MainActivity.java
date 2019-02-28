3405d81fc50c33eb41789bc793e738c7ebccb525
rim <devrealticon.gmail.com>
2019-02-28 17:36:04 +0900

    Initial commit

---
+++ app/src/main/java/com/mrhi2018/ex28activity5/MainActivity.java
@@ -0,0 +1,21 @@
+package com.mrhi2018.ex28activity5;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void clickBtn(View view) {
+        Intent intent= new Intent();
+        intent.setAction("aaa");
+        startActivity(intent);
+    }
+}
